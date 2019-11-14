ab4ab052455c2b35cfa90494740ffa3ca1599bca
Zachary Buchanan <zbuchanan@me.com>
2019-11-14 18:03:48 -0500

    first commit

---
+++ TyperTest/ConfigureViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TyperTest
+//
+//  Created by Zach Buchanan on 11/14/19.
+//  Copyright © 2019 Zach Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class ConfigureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
